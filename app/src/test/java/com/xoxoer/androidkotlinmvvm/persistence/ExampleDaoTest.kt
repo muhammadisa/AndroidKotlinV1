331d7e86c4cacd0ee04eec31f68bd98fabdfb94a
muhammadisa <hyperspace018@gmail.com>
2020-08-06 18:31:46 +0700

    Create testing for room persistence

---
+++ app/src/test/java/com/xoxoer/androidkotlinmvvm/persistence/ExampleDaoTest.kt
@@ -0,0 +1,38 @@
+package com.xoxoer.androidkotlinmvvm.persistence
+
+import com.xoxoer.androidkotlinmvvm.utils.MockUtil
+import com.xoxoer.androidkotlinmvvm.utils.MockUtil.mockExample
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [23])
+class ExampleDaoTest: LocalDatabase() {
+
+    private lateinit var exampleDao: ExampleDao
+
+    @Before
+    fun init(){
+        exampleDao = db.exampleDao()
+    }
+
+    @Test
+    fun insertAndLoadExample() = runBlocking {
+
+        val mockExampleList = mockExample()
+
+        exampleDao.insertExample(mockExample())
+
+        val loadFromDB = exampleDao.getExample(1)!!
+        assertThat(loadFromDB.toString(), `is`(mockExampleList.toString()))
+        assertThat(loadFromDB.id, `is`(1))
+
+    }
+
+}
